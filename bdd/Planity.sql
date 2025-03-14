01fafa51695fde3eb2f3c68e7473219c727f0338
Sacha <sachasimon92350@gmail.com>
2025-03-14 09:27:31 +0100

    add word

--- bdd/Planity.sql
+++ bdd/Planity.sql
@@ -164,7 +164,7 @@ CREATE TABLE public.Users(
     mail         VARCHAR (80) NOT NULL UNIQUE,
     phone        INT NOT NULL UNIQUE,
     zip          VARCHAR (10) NOT NULL,
-    password     VARCHAR (50) NOT NULL,
+    password     VARCHAR (255) NOT NULL,
     created_at   DATE NOT NULL,
     updated_at   DATE NOT NULL,
     id_cookie    INT NOT NULL,

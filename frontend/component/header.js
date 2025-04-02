6485135a36850f64c005812a7931a0a98f6c22b1
Drx93 <dorian93.roux@gmail.com>
2025-04-02 17:19:28 +0100

    add home

---
+++ frontend/component/header.js
@@ -0,0 +1,16 @@
+export default function Header() {
+    return (
+    <>
+    <header>
+            <h1><a>PLANITY</a></h1>
+            <img src="/planity-logo.png"/>
+            <nav>
+                <a>Coiffeur</a>
+                <a>Barbier</a>
+                <a>Manucure</a>
+            </nav>
+            <a><img src="/user.svg"></img> Mon compte</a>
+        </header>
+    </>
+    )
+}

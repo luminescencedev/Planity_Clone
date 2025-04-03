94b7652cdb34dde3b5fb6f5711bbe85e9aa66ff5
Arthur <arthur.garnier1090@gmail.com>
2025-04-03 15:14:37 +0200

    add href header

--- frontend/component/header.js
+++ frontend/component/header.js
@@ -2,13 +2,13 @@ export default function Header() {
     return (
     <>
     <header>
-        <a href="/home"><img src="/planity-logo.png"/></a>
+        <a href="/home"><img src="/planity-logo.png" alt="Planity Logo"/></a>
         <nav>
-                <a>Coiffeur</a>
-                <a>Barbier</a>
-                <a>Manucure</a>
-            </nav>
-            <a href="/login"><img src="/userwhite.svg"></img> Mon compte</a>
+            <a href="/categories/Coiffeur">Coiffeur</a>
+            <a href="/categories/Barbier">Barbier</a>
+            <a href="/categories/Manucure">Manucure</a>
+        </nav>
+        <a href="/login"><img src="/userwhite.svg" alt="User Icon"/> Mon compte</a>
     </header>
     </>
     )

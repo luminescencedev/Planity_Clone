bb15688b6789951e5339fae295f2a5fab361bd71
Pierre Zhou <sharkpi.zhou@gmail.com>
2025-04-03 12:53:50 +0200

    Add header and footer styles; refactor account page styles

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,8 @@
 import { AuthProvider } from "../context/AuthContext";
+import "../styles/header.css";
+import "../styles/footer.css";
 import "../styles/account.css";
+
 import "../styles/global.css";
 
 

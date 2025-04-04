b162451a559ee7cab6c2e7c6d4041779fe146f0e
Drx93 <dorian93.roux@gmail.com>
2025-04-04 12:51:27 +0100

    Update bdd

--- bdd/Planity.sql
+++ bdd/Planity.sql
@@ -62,11 +62,56 @@ CREATE TABLE public.Salons(
     CONSTRAINT Salons_Categories_FK FOREIGN KEY (id_category) REFERENCES public.Categories(id_category)
 )WITHOUT OIDS;
 
--- Insertion des salons
-INSERT INTO public.Salons (name, adress, city, picture, description, created_at, updated_at, id_category)
-VALUES
-('Salon de Paris', '123 Rue de Paris', 'Paris', 'url_to_image', 'Description du salon de Paris', current_date, current_date, 1),
-('Salon de Lyon', '456 Rue de Lyon', 'Lyon', 'url_to_image', 'Description du salon de Lyon', current_date, current_date, 2);
+-- Salons de Coiffeur
+INSERT INTO public.Salons (name, adress, city, picture, description, created_at, updated_at, id_category) VALUES
+('Coiffure Élégance', '1 Rue de Paris', 'Paris', 'image_salon_1.png', 'Salon moderne offrant une coupe stylée et personnalisée selon vos préférences.', current_date, current_date, 1),
+('La Tête au Carré', '2 Rue de Paris', 'Paris', 'image_salon_2.png', 'Un salon réputé pour ses coupes innovantes et son service client exceptionnel.', current_date, current_date, 1),
+('Coupe Précise', '3 Rue de Paris', 'Paris', 'image_salon_3.png', 'Des experts en coiffure créative vous aideront à adopter un style unique et tendance.', current_date, current_date, 1),
+('Lyon Coiffure', '4 Rue de Lyon', 'Lyon', 'image_salon_4.png', 'Nos coiffeurs expérimentés vous offrent une coupe qui respecte vos envies et votre personnalité.', current_date, current_date, 1),
+('Chic et Court', '5 Rue de Lyon', 'Lyon', 'image_salon_5.png', 'Salon où chaque client se voit offrir une coupe moderne et sophistiquée sur mesure.', current_date, current_date, 1),
+('Lyon Style', '6 Rue de Lyon', 'Lyon', 'image_salon_6.png', 'Faites confiance à nos coiffeurs pour un style personnalisé et une coupe parfaite.', current_date, current_date, 1),
+('Marseille Coiffure', '7 Rue de Marseille', 'Marseille', 'image_salon_7.png', 'Venez découvrir notre salon où chaque coupe reflète votre personnalité et vos attentes.', current_date, current_date, 1),
+('Coupes & Styles', '8 Rue de Marseille', 'Marseille', 'image_salon_8.png', 'Le salon où vos cheveux sont pris en main avec expertise pour un look tendance.', current_date, current_date, 1),
+('Marseille Glam', '9 Rue de Marseille', 'Marseille', 'image_salon_9.png', 'Un salon chic à Marseille pour une coupe élégante et un service premium adapté à vous.', current_date, current_date, 1),
+('Bordeaux Hair', '10 Rue de Bordeaux', 'Bordeaux', 'image_salon_10.png', 'Coiffeurs professionnels vous offrant un service sur-mesure pour une coupe parfaite.', current_date, current_date, 1),
+('Le Style Toulousain', '11 Rue de Toulouse', 'Toulouse', 'image_salon_10.png', 'Là où la coupe rencontre l’art, et où chaque client est traité comme une star.', current_date, current_date, 1),
+('Strasbourg hair', '12 Rue de Strasbourg', 'Strasbourg', 'image_salon_10.png', 'Le salon pour ceux qui recherchent une coupe de cheveux raffinée et soignée.', current_date, current_date, 1),
+('Éclat Coiffure', '13 Rue de Nice', 'Nice', 'image_salon_10.png', 'Coiffeurs experts pour une coupe qui respecte à la fois la mode et votre personnalité.', current_date, current_date, 1),
+('Salon Prestige', '14 Rue de Lille', 'Lille', 'image_salon_10.png', 'Chez nous, chaque client est assuré de repartir avec une coupe qui lui va parfaitement.', current_date, current_date, 1),
+('L’Atelier Coiffure', '15 Rue de Nantes', 'Nantes', 'image_salon_10.png', 'Un salon où la coupe n’est pas juste un métier, mais une véritable passion.', current_date, current_date, 1);
+
+-- Salons de Barbier
+INSERT INTO public.Salons (name, adress, city, picture, description, created_at, updated_at, id_category) VALUES
+('Barbier Royal', '1 Rue de Paris', 'Paris', 'image_salon_16.png', 'Un barbier classique avec une touche moderne, parfait pour une coupe soignée et élégante.', current_date, current_date, 2),
+('La Barbe Élégante', '2 Rue de Paris', 'Paris', 'image_salon_17.png', 'Des soins de barbe et de cheveux dans un cadre intime et élégant à Paris.', current_date, current_date, 2),
+('Barbe & Style', '3 Rue de Paris', 'Paris', 'image_salon_18.png', 'Le barbier tendance où chaque détail de votre coupe de barbe est maîtrisé à la perfection.', current_date, current_date, 2),
+('Lyon Barbier', '4 Rue de Lyon', 'Lyon', 'image_salon_19.png', 'Barbier traditionnel avec une approche moderne pour des barbes impeccables et stylées.', current_date, current_date, 2),
+('La Maison de la Barbe', '5 Rue de Lyon', 'Lyon', 'image_salon_20.png', 'Venez découvrir un salon de barbier où l’art de tailler la barbe se transforme en rituel.', current_date, current_date, 2),
+('Barbier & Co', '6 Rue de Lyon', 'Lyon', 'image_salon_21.png', 'Un barbier dynamique et créatif, offrant des services de qualité pour une barbe parfaite.', current_date, current_date, 2),
+('Marseille Barbe', '7 Rue de Marseille', 'Marseille', 'image_salon_22.png', 'Des experts du rasage et du soin de la barbe pour une expérience inoubliable.', current_date, current_date, 2),
+('Le Barbier Marseillais', '8 Rue de Marseille', 'Marseille', 'image_salon_23.png', 'Un salon où la coupe de barbe devient une véritable œuvre d’art, à Marseille.', current_date, current_date, 2),
+('Barbier du Sud', '9 Rue de Marseille', 'Marseille', 'image_salon_24.png', 'Offrez à votre barbe une coupe soignée et moderne dans un cadre agréable et raffiné.', current_date, current_date, 2),
+('Barber Lounge', '10 Rue de Nice', 'Nice', 'image_salon_10.png', 'Un salon de barbier où chaque service est effectué avec précision et passion pour la barbe.', current_date, current_date, 2),
+('Lyon Barbier Chic', '11 Rue de Toulouse', 'Toulouse', 'image_salon_10.png', 'Rasages et tailles de barbe réalisés par des professionnels pour un look impeccable.', current_date, current_date, 2),
+('Le Barbier Moderne', '12 Rue de Strasbourg', 'Strasbourg', 'image_salon_10.png', 'Redécouvrez l’art du rasage traditionnel avec des produits de qualité supérieure.', current_date, current_date, 2),
+('Barbier et Tradition', '13 Rue de Bordeaux', 'Bordeaux', 'image_salon_10.png', 'Des services de rasage et coupe de barbe dans un environnement traditionnel et moderne.', current_date, current_date, 2),
+('Barbe & Chic', '14 Rue de Lille', 'Lille', 'image_salon_10.png', 'Barbier classique et moderne pour des prestations de qualité adaptées à tous les styles.', current_date, current_date, 2),
+('Le Barbier nanterois', '15 Rue de Nantes', 'Nantes', 'image_salon_10.png', 'Un salon de barbier à la fois élégant et accueillant, pour un rasage à la perfection.', current_date, current_date, 2);
+
+-- Salons de Manucure
+INSERT INTO public.Salons (name, adress, city, picture, description, created_at, updated_at, id_category) VALUES
+('Manucure Parisienne', '1 Rue de Paris', 'Paris', 'image_salon_31.png', 'Des manucures élégantes et raffinées dans un salon chic au cœur de Paris.', current_date, current_date, 3),
+('Les Ongles de Paris', '2 Rue de Paris', 'Paris', 'image_salon_32.png', 'Un service de manucure haut de gamme pour des mains impeccables et bien entretenues.', current_date, current_date, 3),
+('Nantes Nail Studio', '3 Rue de Nantes', 'Nantes', 'image_salon_10.png', 'Salon moderne spécialisé dans les manucures tendances avec des soins de qualité supérieure.', current_date, current_date, 3),
+('Manucure Lyon', '4 Rue de Lyon', 'Lyon', 'image_salon_34.png', 'Une manucure parfaite avec des produits naturels dans une ambiance détendue à Lyon.', current_date, current_date, 3),
+('Ongles et Éclat', '5 Rue de Lille', 'Lille', 'image_salon_10.png', 'Pour des mains soignées et des ongles éclatants, notre salon vous offre des soins personnalisés.', current_date, current_date, 3),
+('Lyon Nail Art', '6 Rue de Lyon', 'Lyon', 'image_salon_36.png', 'Le salon incontournable pour des manucures créatives et des soins de qualité supérieure.', current_date, current_date, 3),
+('Marseille Ongles', '7 Rue de Marseille', 'Marseille', 'image_salon_37.png', 'Un endroit chic à Marseille pour des manucures artistiques et des ongles parfaits.', current_date, current_date, 3),
+('Manucure Glamour', '8 Rue de Marseille', 'Marseille', 'image_salon_10.png', 'Un salon où chaque client bénéficie d’un service de manucure de qualité, avec des produits de luxe.', current_date, current_date, 3),
+('Luxe Nails', '9 Rue de Strasbourg', 'Strasbourg', 'image_salon_10.png', 'Ongles parfaits et manucure de qualité dans un cadre haut de gamme et agréable.', current_date, current_date, 3),
+('Bordeaux Nail Spa', '10 Rue de Bordeaux', 'Bordeaux', 'image_salon_10.png', 'Venez vivre une expérience relaxante et beauté avec des manucures et soins de luxe à Bordeaux.', current_date, current_date, 3),
+('Toulouse Nail Lounge', '11 Rue de Toulouse', 'Toulouse', 'image_salon_10.png', 'Offrez à vos mains des manucures parfaites et des soins adaptés à vos besoins à Toulouse.', current_date, current_date, 3),
+('Nice Nails Studio', '12 Rue de Nice', 'Nice', 'image_salon_10.png', 'Nous offrons des manucures de qualité dans un espace moderne et accueillant à Nice', current_date, current_date, 3);
 
 ------------------------------------------------------------
 -- Table: Services
@@ -87,7 +132,90 @@ CREATE TABLE public.Services(
 INSERT INTO public.Services (price, time, description, created_at, updated_at, id_salon)
 VALUES
 (30.00, 60, 'Coupe de cheveux', current_date, current_date, 1),
-(20.00, 45, 'Rasage de barbe', current_date, current_date, 2);
+(30.00, 60, 'Coupe de cheveux', current_date, current_date, 2),
+(30.00, 60, 'Coupe de cheveux', current_date, current_date, 3),
+(30.00, 60, 'Coupe de cheveux', current_date, current_date, 4),
+(30.00, 60, 'Coupe de cheveux', current_date, current_date, 5),
+(30.00, 60, 'Coupe de cheveux', current_date, current_date, 6),
+(30.00, 60, 'Coupe de cheveux', current_date, current_date, 7),
+(30.00, 60, 'Coupe de cheveux', current_date, current_date, 8),
+(30.00, 60, 'Coupe de cheveux', current_date, current_date, 9),
+(30.00, 60, 'Coupe de cheveux enfant', current_date, current_date, 1),
+(30.00, 60, 'Coupe de cheveux enfant', current_date, current_date, 2),
+(30.00, 60, 'Coupe de cheveux enfant', current_date, current_date, 3),
+(30.00, 60, 'Coupe de cheveux enfant', current_date, current_date, 4),
+(30.00, 60, 'Coupe de cheveux enfant', current_date, current_date, 5),
+(30.00, 60, 'Coupe de cheveux enfant', current_date, current_date, 6),
+(30.00, 60, 'Coupe de cheveux enfant', current_date, current_date, 7),
+(30.00, 60, 'Coupe de cheveux enfant', current_date, current_date, 8),
+(30.00, 60, 'Coupe de cheveux enfant', current_date, current_date, 9),
+(30.00, 60, 'Coupe de cheveux', current_date, current_date, 10),
+(30.00, 60, 'Coupe de cheveux', current_date, current_date, 11),
+(30.00, 60, 'Coupe de cheveux', current_date, current_date, 12),
+(30.00, 60, 'Coupe de cheveux', current_date, current_date, 13),
+(30.00, 60, 'Coupe de cheveux', current_date, current_date, 14),
+(30.00, 60, 'Coupe de cheveux', current_date, current_date, 15),
+(30.00, 60, 'Coupe de cheveux', current_date, current_date, 16),
+(30.00, 60, 'Coupe de cheveux', current_date, current_date, 17),
+(30.00, 60, 'Coupe de cheveux', current_date, current_date, 18),
+(30.00, 60, 'Coupe de cheveux', current_date, current_date, 19),
+(30.00, 60, 'Coupe de cheveux', current_date, current_date, 20),
+(30.00, 60, 'Coupe de cheveux', current_date, current_date, 21),
+(30.00, 60, 'Coupe de cheveux', current_date, current_date, 22),
+(30.00, 60, 'Coupe de cheveux', current_date, current_date, 23),
+(30.00, 60, 'Coupe de cheveux', current_date, current_date, 24),
+(30.00, 60, 'Coupe de cheveux', current_date, current_date, 25),
+(30.00, 60, 'Coupe de cheveux', current_date, current_date, 26),
+(30.00, 60, 'Coupe de cheveux', current_date, current_date, 27),
+(30.00, 60, 'Coupe de cheveux', current_date, current_date, 28),
+(30.00, 60, 'Coupe de cheveux', current_date, current_date, 29),
+(30.00, 60, 'Coupe de cheveux', current_date, current_date, 30),
+(30.00, 60, 'Coupe de cheveux', current_date, current_date, 31),
+(30.00, 60, 'Coupe de cheveux', current_date, current_date, 32),
+(30.00, 60, 'Coupe de cheveux', current_date, current_date, 33),
+(30.00, 60, 'Coupe de cheveux', current_date, current_date, 34),
+(20.00, 45, 'Rasage de barbe', current_date, current_date, 2),
+(20.00, 45, 'Rasage de barbe', current_date, current_date, 8),
+(20.00, 45, 'Rasage de barbe', current_date, current_date, 7),
+(20.00, 45, 'Rasage de barbe', current_date, current_date, 6),
+(20.00, 45, 'Rasage de barbe', current_date, current_date, 5),
+(20.00, 45, 'Rasage de barbe', current_date, current_date, 4),
+(20.00, 45, 'Rasage de barbe', current_date, current_date, 3),
+(20.00, 45, 'Rasage de barbe', current_date, current_date, 2),
+(20.00, 45, 'Rasage de barbe', current_date, current_date, 1),
+(20.00, 45, 'Taillage barbe', current_date, current_date, 16),
+(20.00, 45, 'Taillage barbe', current_date, current_date, 17),
+(20.00, 45, 'Taillage barbe', current_date, current_date, 18),
+(20.00, 45, 'Taillage barbe', current_date, current_date, 19),
+(20.00, 45, 'Taillage barbe', current_date, current_date, 20),
+(20.00, 45, 'Taillage barbe', current_date, current_date, 21),
+(20.00, 45, 'Taillage barbe', current_date, current_date, 22),
+(20.00, 45, 'Taillage barbe', current_date, current_date, 23),
+(20.00, 45, 'Taillage barbe', current_date, current_date, 24),
+(15.00, 25, 'Pose ongle', current_date, current_date, 31),
+(15.00, 25, 'Pose ongle', current_date, current_date, 32),
+(15.00, 25, 'Pose ongle', current_date, current_date, 33),
+(15.00, 25, 'Pose ongle', current_date, current_date, 34),
+(15.00, 25, 'Pose ongle', current_date, current_date, 35),
+(15.00, 25, 'Pose ongle', current_date, current_date, 36),
+(15.00, 25, 'Pose ongle', current_date, current_date, 37),
+(15.00, 25, 'Pose ongle', current_date, current_date, 38),
+(15.00, 25, 'Pose ongle', current_date, current_date, 39),
+(15.00, 25, 'Pose ongle', current_date, current_date, 40),
+(15.00, 25, 'Pose ongle', current_date, current_date, 41),
+(15.00, 25, 'Pose ongle', current_date, current_date, 42),
+(27.00, 35, 'Brushing', current_date, current_date, 31),
+(27.00, 35, 'Brushing', current_date, current_date, 32),
+(27.00, 35, 'Brushing', current_date, current_date, 33),
+(27.00, 35, 'Brushing', current_date, current_date, 34),
+(27.00, 35, 'Brushing', current_date, current_date, 35),
+(27.00, 35, 'Brushing', current_date, current_date, 36),
+(27.00, 35, 'Brushing', current_date, current_date, 37),
+(27.00, 35, 'Brushing', current_date, current_date, 38),
+(27.00, 35, 'Brushing', current_date, current_date, 39),
+(27.00, 35, 'Brushing', current_date, current_date, 40),
+(27.00, 35, 'Brushing', current_date, current_date, 41),
+(27.00, 35, 'Brushing', current_date, current_date, 42);
 
 ------------------------------------------------------------
 -- Table: Reviews
